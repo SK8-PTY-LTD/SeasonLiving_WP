694e94566322b339a1408ad1655fb3e3ceb05883
Dwight <baomp2015@gmail.com>
2019-03-06 15:20:23 +1100

    customised admin page

--- shopkeeper-child/functions.php
+++ shopkeeper-child/functions.php
@@ -26,3 +26,55 @@ function wc_hide_selected_terms( $terms, $taxonomies, $args ) {
 	}
 	return $terms;
 }
+
+// BEGIN DASHBOARD LOGO CUSTOMISATION
+function wpb_custom_logo() {
+	echo '
+		<style type="text/css">
+			#wpadminbar #wp-admin-bar-wp-logo > .ab-item .ab-icon:before {
+				background-image: url(' . get_stylesheet_directory_uri() . '/images/custom-logo.png) !important;
+				background-position: 0 0;
+                color:rgba(0, 0, 0, 0);
+			}
+			#wpadminbar #wp-admin-bar-wp-logo.hover > .ab-custom-logoitem .ab-icon {
+				background-position: 0 0;
+			}
+		</style>
+	';
+}
+ 
+//hook into the administrative header output
+add_action('wp_before_admin_bar_render', 'wpb_custom_logo');
+
+// END DASHBOARD LOGO CUSTOMISATION 
+
+// BEGIN LOGIN PAGE CUSTOMISATION 
+function my_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/site-login-logo.png);
+            height:87px;
+            width:206px;
+            background-size: 206px 87px;
+            background-repeat: no-repeat;
+        	padding-bottom: 30px;
+        }
+        #loginform {
+            border-radius: 5px;
+            box-shadow: 0 0 12px #767676;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
+function my_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+    return 'Your Site Name and Info';
+}
+add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+
+// END LOGIN PAGE CUSTOMISATION 
